6f927397da2cd55d92b320f782bd1e01e31f1910
shubh2928 <theownershubh@gmail.com>
2025-02-10 22:06:11 +0530

    Update new.py

--- new.py
+++ new.py
@@ -498,7 +498,7 @@ def handle_bgmi(message):
             target = command[1]
             port = int(command[2])  # @PrivateFileTg # Convert time to integer
             time = int(command[3])  # @PrivateFileTg # Convert port to integer
-            if time > 240:
+            if time > 120:
                 response = "Error: Time interval must be less than 180."
             else:
                 record_command_logs(user_id, '/bgmi', target, port, time)
